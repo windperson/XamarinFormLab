e2e9f55159c2f8d66cfdfa19115e0f6f0d4acd58
windperson <windperson@gmail.com>
2016-11-19 14:45:29 +0800

    Lab 3, using Data Binding in Xamarin.Form

--- XamarinDevDayLab/XamarinDevDayLab/MainPage.xaml.cs
+++ XamarinDevDayLab/XamarinDevDayLab/MainPage.xaml.cs
@@ -7,34 +7,15 @@ using Xamarin.Forms;
 
 namespace XamarinDevDayLab
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : CarouselPage
     {
         public MainPage()
         {
             InitializeComponent();
-            this.AgeInput.ValueChanged += AgeInput_ValueChanged;
-            this.SaveBtn.Clicked += SaveBtn_Clicked;
-            this.DialBtn.Clicked += DialBtn_Clicked;
+            var employeeService = new Services.EmployService();
+            ItemsSource = employeeService.GetAllEmployees();
+            
         }
 
-        private async void DialBtn_Clicked(object sender, EventArgs e)
-        {
-            var dialStr = this.DialInput.Text;
-            if(!String.IsNullOrEmpty(dialStr) 
-                && await this.DisplayAlert("Dial Out","Call out to: \""+ this.DialInput.Text + "\" ?","YES","NO" ))
-            {
-                Device.OpenUri(new Uri("tel://"+dialStr));
-            }
-        }
-
-        private void SaveBtn_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Message", "Save Data", "OK");
-        }
-
-        private void AgeInput_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            this.AgeVal.Text = this.AgeInput.Value.ToString("N0");
-        }
     }
 }
---
+++ XamarinDevDayLab/XamarinDevDayLab/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinDevDayLab.Models
+{
+    public class Employee
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public String Phone { get; set; }
+    }
+}
