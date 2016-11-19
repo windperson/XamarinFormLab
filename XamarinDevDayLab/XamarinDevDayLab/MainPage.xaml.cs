ac7ae6400ec0df19d4876f5762560aed512c23ed
windperson <windperson@gmail.com>
2016-11-19 14:02:02 +0800

    Lab1

--- XamarinDevDayLab/XamarinDevDayLab/MainPage.xaml.cs
+++ XamarinDevDayLab/XamarinDevDayLab/MainPage.xaml.cs
@@ -12,6 +12,18 @@ namespace XamarinDevDayLab
         public MainPage()
         {
             InitializeComponent();
+            this.AgeInput.ValueChanged += AgeInput_ValueChanged;
+            this.SaveBtn.Clicked += SaveBtn_Clicked;
+        }
+
+        private void SaveBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Message", "Save Data", "OK");
+        }
+
+        private void AgeInput_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.AgeVal.Text = this.AgeInput.Value.ToString("N0");
         }
     }
 }
