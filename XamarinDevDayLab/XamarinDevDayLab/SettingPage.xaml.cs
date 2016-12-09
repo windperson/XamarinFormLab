44ee1453ba19fde55305d5eee06a992c0e74ce54
windperson <windperson@gmail.com>
2016-12-09 20:07:24 +0800

    eliminate compile warning.

--- XamarinDevDayLab/XamarinDevDayLab/SettingPage.xaml.cs
+++ XamarinDevDayLab/XamarinDevDayLab/SettingPage.xaml.cs
@@ -11,67 +11,67 @@ using XamarinDevDayLab.Models;
 
 namespace XamarinDevDayLab
 {
-    public partial class SettingPage : ContentPage
-    {
-        public SettingPage()
-        {
-            InitializeComponent();
-            
-            this.orientationBtn.Clicked += OrientationBtn_Clicked;
-            this.cameraBtn.Clicked += async (object sender, EventArgs e) =>
-            {
-                //fix for running on UWP, see http://stackoverflow.com/a/40493013/1075882
-                var libStatus = await CrossMedia.Current.Initialize();
-                if (!libStatus)
-                {
-                    DisplayAlert("lib init", "lib init error", "OK");
-                    return;
-                }
-                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-                {
-                    DisplayAlert("No Camera", "No Camera available", "OK");
-                    return;
-                }
-                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-                {
-                    Directory = "Sample",
-                    Name = "test.jpg"
-                });
+	public partial class SettingPage : ContentPage
+	{
+		public SettingPage()
+		{
+			InitializeComponent();
+			
+			this.orientationBtn.Clicked += OrientationBtn_Clicked;
+			this.cameraBtn.Clicked += async (object sender, EventArgs e) =>
+			{
+				//fix for running on UWP, see http://stackoverflow.com/a/40493013/1075882
+				var libStatus = await CrossMedia.Current.Initialize();
+				if (!libStatus)
+				{
+					await DisplayAlert("lib init", "lib init error", "OK");
+					return;
+				}
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+				{
+					await DisplayAlert("No Camera", "No Camera available", "OK");
+					return;
+				}
+				var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{
+					Directory = "Sample",
+					Name = "test.jpg"
+				});
 
-                if (file == null)
-                {
-                    //TODO: should notify that take "no" picture to user?
-                    return;
-                }
-                DisplayAlert("Pic file location", file.Path, "OK");
+				if (file == null)
+				{
+					//TODO: should notify that take "no" picture to user?
+					return;
+				}
+				await DisplayAlert("Pic file location", file.Path, "OK");
 
-                this.myImg.Source = ImageSource.FromStream(() =>
-                {
-                    var stream = file.GetStream();
-                    file.Dispose();
-                    return stream;
-                });
-            };
+				this.myImg.Source = ImageSource.FromStream(() =>
+				{
+					var stream = file.GetStream();
+					file.Dispose();
+					return stream;
+				});
+			};
 
-        }
+		}
 
-        private void OrientationBtn_Clicked(object sender, EventArgs e)
-        {
+		private void OrientationBtn_Clicked(object sender, EventArgs e)
+		{
 			var orientService = DependencyService.Get<IDeviceOrientation>();
 			var orientation = orientService == null ? DeviceOrientations.Undefined : orientService.GetOrientation();
-            switch (orientation)
-            {
-                case DeviceOrientations.Undefined:
-                    DisplayAlert("Message", "Undefined?!", "OK");
-                    break;
-                case DeviceOrientations.Landscape:
-                    DisplayAlert("Message", "Landscape!", "OK");
-                    break;
-                case DeviceOrientations.Portrait:
-                    DisplayAlert("Message", "Portrait!", "OK");
-                    break;
-            }
+			switch (orientation)
+			{
+				case DeviceOrientations.Undefined:
+					DisplayAlert("Message", "Undefined?!", "OK");
+					break;
+				case DeviceOrientations.Landscape:
+					DisplayAlert("Message", "Landscape!", "OK");
+					break;
+				case DeviceOrientations.Portrait:
+					DisplayAlert("Message", "Portrait!", "OK");
+					break;
+			}
 
-        }
-    }
+		}
+	}
 }
